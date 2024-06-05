0b671af741a5299665b79cec1b602c824778b994
sjcalabrese <erotavlas103@gmail.com>
2024-06-05 17:26:07 -0400

    Showing that the app function only gets executed once, and how not to update the UI

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -6,10 +6,14 @@ import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
+  let tabContent = 'Please click a button';
+
   function handleSelect(selectedButton) {
     //selectedButton => 'components', 'jsx', 'props', 'state'
     console.log(selectedButton);
+    tabContent = selectedButton;
   }
+  console.log('App component only renders once :)')
   return (
     <div>
       <Header></Header>
@@ -37,7 +41,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {tabContent}
         </section>
       </main>
     </div>
