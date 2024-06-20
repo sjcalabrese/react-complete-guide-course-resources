6b092f9e0429671de02c0e91883f98e4f2063502
sjcalabrese <erotavlas103@gmail.com>
2024-06-20 16:28:05 -0400

    Updating functionality to insert x's on gameBoard

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/gameBoard/GameBoard.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/gameBoard/GameBoard.jsx
@@ -6,14 +6,27 @@ const initialGameBoard = [
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      // Create a copy of the current board and then update it in order to prevent
+      // strange issues, similar to how you would update useState
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
