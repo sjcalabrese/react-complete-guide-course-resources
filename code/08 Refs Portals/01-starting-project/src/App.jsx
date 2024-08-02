0d4d29d147eec75ccb3e4bd80e5a5d18d6f75fb9
sjcalabrese <erotavlas103@gmail.com>
2024-08-02 16:51:51 -0400

    creating timechallenge component

--- code/08 Refs Portals/01-starting-project/src/App.jsx
+++ code/08 Refs Portals/01-starting-project/src/App.jsx
@@ -1,10 +1,15 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimeChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
       <div id="challenges"></div>
+      <TimerChallenge title="Easy" targetTime={1}/>
+      <TimerChallenge title="Not easy" targetTime={5}/>
+      <TimerChallenge title="Getting Tough" targetTime={10}/>
+      <TimerChallenge title="Pros Only" targetTime={15}/>
     </>
   );
 }
