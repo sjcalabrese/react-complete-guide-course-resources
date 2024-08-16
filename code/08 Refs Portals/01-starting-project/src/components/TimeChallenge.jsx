b39f853bf1ad8703576b2159af94b850984b57c8
sjcalabrese <erotavlas103@gmail.com>
2024-08-16 16:08:59 -0400

    creating handleStop function.

--- code/08 Refs Portals/01-starting-project/src/components/TimeChallenge.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/TimeChallenge.jsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+//let timer;
+/* cannot use a global veriable as a solution to stop the timer becuase if you try
+2 timers at the same time, the firs timer will be thrown away. Resulting in th first time not being stopped
+when you attempt to stop it.
+*/
+//using a ref could be a solution. Then use timer.current to make it component instance specific
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
+  
 
   function handleStart() {
     console.log("Timer started");
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
     setTimerStarted(true);
+  }
+
+  function handleStop() {
+    console.log("Timer stopped");
+    clearTimeout(timer.current);
+    setTimerStarted(false);
   }
 
   return (
@@ -20,7 +35,7 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
