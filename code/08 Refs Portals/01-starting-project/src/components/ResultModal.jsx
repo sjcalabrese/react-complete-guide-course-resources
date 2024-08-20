a90681b05ec6a12c2e14c4ae361291ecf680eff6
sjcalabrese <erotavlas103@gmail.com>
2024-08-20 15:50:30 -0400

    Updating to have modal show scores or whther you lost.

--- code/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
@@ -3,11 +3,14 @@ import { forwardRef, useImperativeHandle, useRef } from "react";
 //when structured like this you will then received a second argument (ref) outside the props argument
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { result, targetTime, remainingTime, onReset },
   ref
 ) {
-
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   const dialog = useRef();
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -18,14 +21,15 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds</strong>.
       </p>
       <p>
-        You Stopped the timer with <strong>X seconds left</strong>
+        You Stopped the timer with <strong>{formattedRemainingTime}</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
